1b05cb53c807ca4e03ee253b637d5920a3a4d235
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-09-16 21:18:47 +0200

    feat: add new exercises for function creation and destructuring in script.js
    - Implement spellWord function to log each letter of a string with spaces
    - Add destructuring examples for keywords and publisher properties from the books array
    - Introduce printBookAuthorsCount function to log the number of authors for a given book title

---  Assignments lecture 9/script.js
+++  Assignments lecture 9/script.js
@@ -293,8 +293,31 @@ console.log(bookAuthors)
 // Write a function called spellWord that accepts a single string as an argument.This function should log to the console each letter of the argument separated by a space.
 
 
-spellWord('Javascript')
+
 const spellWord = function(word){
     console.log(...word);
 }
+spellWord('Javascript')
 
+
+
+// Rest Pattern and Parameters
+// 4.1﻿
+// Destructure the keywords property(array) of the first book from the books array into variables called mainKeyword and rest.The first keyword should be assigned to mainKeyword, and the rest of the keywords should be assigned to the rest variable(it should be an array).
+
+const [mainKeyword, ...rest]  = books[0].keywords
+console.log(rest, mainKeyword);
+
+// 4.2﻿
+// Destructure the second book from the books array into a variable called bookPublisher.The bookPublisher variable should be assigned with the value of the publisher property of the book object.Assign the rest of the properties to the restOfTheBook variable.
+
+const {publisher: bookPublisher, ...restOfTheBook} = books[1];
+console.log(bookPublisher, restOfTheBook); 
+
+// 4.3﻿
+// Write a function called printBookAuthorsCount that has two parameters called title and authors.The authors parameter should accept any number of arguments.This function should log to the console a string formatted like that: "The book "${ title } " has ${authors.length} authors".
+
+const printBookAuthorsCount = function(title, ...authors){
+    console.log(`The book "${title}" has ${authors.length} authors`)
+}
+printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
