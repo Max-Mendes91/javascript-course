bb6ebe6a1177a1294e5fa86304a485a0b93c3902
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-09-17 20:35:45 +0200

    feat: add logical assignment operators for book properties in script.js
    - Implement logical assignment to set default edition for books missing the property
    - Update highlighted property based on Goodreads rating using logical operators
    - Enhance script.js with new exercises demonstrating practical use of logical assignments

---  Assignments lecture 9/script.js
+++  Assignments lecture 9/script.js
@@ -352,3 +352,20 @@ for (let i = 0; i < books.length; i++){
 for (let i = 0; i < books.length; i++){
     books[i].onlineContent ?? console.log(`${title}" provides no data about its online content`)
 }
+
+
+// Logical Assignments Operators
+// 7.1﻿
+// Some of the book objects from the books array are missing the edition property.Loop over the books array, and assign this property with a number 1(if it doesn't already exist). Use logical assignment operators.
+
+for (let i = 0; i < books.length; i++) {
+    console.log(books[i].edition ||= 1)
+};
+
+// 7.2﻿
+// Some of the book objects from the books array have the highlighted property, which by default is set to true.Iterate over the books array, and if the thirdParty.goodreads.rating property is less than 4.2, reassign it with false.
+
+// Use the &&= operator(tip: you may also need the! operator)
+for (let i = 0; i < books.length; i++) {
+  console.log(books[i].highlighted &&= !(books[i].thirdParty.goodreads.rating < 4.2))
+}
