641efd36b5f8945f854dcede0b7b15ac94762988
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-09-19 22:10:59 +0200

    feat: add new exercises for object manipulation and key extraction in script.js
    - Introduce exercises utilizing Object.keys() and Object.values() methods to manipulate book data
    - Implement logic to extract keys and values from the thirdParty.goodreads property of the first book
    - Enhance comments for clarity and guidance on the new tasks

---  Assignments lecture 9/script.js
+++  Assignments lecture 9/script.js
@@ -1,5 +1,5 @@
 //Data Structures, Modern Operators and Strings
-/* 
+
 const books = [
     {
         title: 'Algorithms',
@@ -183,7 +183,7 @@ const books = [
         highlighted: true
     }
 ];
-
+/* 
 //1.1 - Destructure the books array into two variables called firstBook and secondBook.
 const [firstBook, secondBook] = books;
 console.log(firstBook, secondBook);
@@ -566,7 +566,6 @@ BONUS: Create an object called 'scorers' which contains the names of the players
       }
 
 GOOD LUCK 😀
-*/
 
 const game = {
     team1: 'Bayern Munich',
@@ -639,3 +638,40 @@ for (const p of game.scored) {
 }
 console.log(playerNames);
 
+*/
+
+// 11.1﻿
+// Below is the entries variable that stores an empty array.Use the for-of loop together with the Object.keys() method to loop over the thirdParty.goodreads property(array) of the first book object from the books array.For each key, push a new array that contains that key to the entries array.
+
+// In the end, the entries array should be filled with arrays containing keys:
+
+
+const entries = [];
+for (const key of Object.keys(books[0].thirdParty.goodreads)){
+    entries.push([key])
+    console.log(key)
+}
+
+// 11.2﻿
+// The Object.values() method returns an array, which means you can call the Array's entries() method on it, for example, Object.entries(books[0].thirdParty.goodreads).entries(). The Array's entries() method returns[index, value] arrays for each element in the array.
+
+// Use the for-of loop together with the Object.values() method and Array's entries() method to loop over thirdParty.goodreads property of the first book from the books array.
+
+// Push each value to the appropriate inner array in the entries array(use index from entries()).
+
+for (const [index, value] of Object.values(books[0].thirdParty.goodreads).entries()){
+    entries[index].push(value);
+    console.log(index, value)
+}
+
+// 11.3﻿
+// Use the Object.entries() method on the thirdParty.goodreads property of the first book from the books array.Assign the returned value to the variable called entries2.
+
+
+const entries2 = Object.entries(books[0].thirdParty.goodreads)
+console.log(entries2)
+
+// 11.4﻿
+// Log the entries and entries2 variables to the console, and compare them.They should look the same.
+
+console.log(`${entries}, ${entries2}`);
