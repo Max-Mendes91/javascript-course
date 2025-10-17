1ae0b3318145ed41dc89d034d4f4f9b8ceb67c36
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-10-17 17:20:18 +0200

    feat: implement ES6 class structure for car with chaining methods
    - Introduce a new ES6 class `carCL` to encapsulate car properties and methods, including private charge property.
    - Implement chaining for `accelerate` and `chargeBattery` methods, enhancing functionality and usability.
    - Update the `brake` method in the parent class for improved performance.
    - Add comments to clarify the coding challenge requirements and provide examples of usage with instances of `carCL`.

--- assignments lecture 14/script.js
+++ assignments lecture 14/script.js
@@ -92,7 +92,7 @@ console.log(ford);
 DATA CAR 1: 'Tesla' going at 120 km / h, with a charge of 23 %
 
 GOOD LUCK 😀 
-*/
+
 
 const Car = function (make, speed) {
     this.make = make;
@@ -131,3 +131,46 @@ tesla.chargeBaterry(90);
 console.log(tesla);
 tesla.brake();
 tesla.accelerate();
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+class carCL {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate = function () { // correct spelling
+        this.speed += 10;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+
+    brake = function () {
+        this.speed -= 5;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+
+
+
+    get speedUS() {
+        return this.speed / 1.6
+    }
+
+    set speedUS(speed) {
+        this.speed = speed * 1.6
+
+    }
+}
+
+const ford = new carCL('Ford', 120)
