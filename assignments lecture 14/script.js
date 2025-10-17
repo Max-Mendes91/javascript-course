5d880b0f5525846ba444755656fb9829851db255
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-10-17 17:37:09 +0200

    refactor: standardize formatting and improve class structure in script.js
    - Adjust indentation and spacing for consistency across the Car and Electric Car classes.
    - Refactor method definitions to use standard ES6 syntax for better readability.
    - Enhance the chaining functionality in the accelerate and chargeBattery methods.
    - Update comments for clarity and maintainability, ensuring alignment with coding challenge requirements.

--- assignments lecture 14/script.js
+++ assignments lecture 14/script.js
@@ -1,6 +1,6 @@
 // Coding Challenge #1
 
- /* 
+/* 
 1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
@@ -12,18 +12,18 @@ DATA CAR 2: 'Mercedes' going at 95 km/h
 GOOD LUCK 😀 
 
 const Car = function (make, speed) {
-    this.make = make;
-    this.speed = speed;
+   this.make = make;
+   this.speed = speed;
 };
 
 Car.prototype.accelerate = function () { // correct spelling
-    this.speed += 10;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
+   this.speed += 10;
+   console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
 Car.prototype.brake = function () {
-    this.speed -= 5;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
+   this.speed -= 5;
+   console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
 const bmw = new Car('BMW', 120);
@@ -48,31 +48,31 @@ GOOD LUCK 😀
 
 
 class carCL {
-    constructor(make, speed) {
-        this.make = make;
-        this.speed = speed;
-    }
+   constructor(make, speed) {
+       this.make = make;
+       this.speed = speed;
+   }
 
-    accelerate = function () { // correct spelling
-        this.speed += 10;
-        console.log(`${this.make} is going at ${this.speed} km/h`);
-    }
+   accelerate = function () { // correct spelling
+       this.speed += 10;
+       console.log(`${this.make} is going at ${this.speed} km/h`);
+   }
 
-    brake = function () {
-        this.speed -= 5;
-        console.log(`${this.make} is going at ${this.speed} km/h`);
-    }
+   brake = function () {
+       this.speed -= 5;
+       console.log(`${this.make} is going at ${this.speed} km/h`);
+   }
 
 
 
-    get speedUS(){
-        return this.speed / 1.6 
-    }
+   get speedUS(){
+       return this.speed / 1.6 
+   }
 
-    set speedUS(speed){
-        this.speed = speed *  1.6
+   set speedUS(speed){
+       this.speed = speed *  1.6
 
-    }
+   }
 }
 
 const ford = new carCL('Ford', 120)
@@ -95,36 +95,36 @@ GOOD LUCK 😀
 
 
 const Car = function (make, speed) {
-    this.make = make;
-    this.speed = speed;
+   this.make = make;
+   this.speed = speed;
 };
 
 Car.prototype.accelerate = function () { // correct spelling
-    this.speed += 10;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
+   this.speed += 10;
+   console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
 Car.prototype.brake = function () {
-    this.speed -= 5;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
+   this.speed -= 5;
+   console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
 const EV = function(make, speed, charge){
-    Car.call(this, make, speed)
-    this.charge = charge;
+   Car.call(this, make, speed)
+   this.charge = charge;
 }
 
 
 EV.prototype = Object.create(Car.prototype);
 
 EV.prototype.chargeBaterry = function (chargeTo){
-    this.charge = chargeTo;
+   this.charge = chargeTo;
 }
 
 EV.prototype.accelerate = function (){
-    this.speed += 20;
-    this.charge--;
-    console.log(`${this.make} is going at ${this.speed} km/h, with charge of ${this.charge}`);
+   this.speed += 20;
+   this.charge--;
+   console.log(`${this.make} is going at ${this.speed} km/h, with charge of ${this.charge}`);
 }
 const tesla = new EV('Tesla', 120, 23)
 tesla.chargeBaterry(90);
@@ -145,32 +145,66 @@ DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
 GOOD LUCK 😀
 */
 
-class carCL {
+class CarCl {
     constructor(make, speed) {
         this.make = make;
         this.speed = speed;
     }
 
-    accelerate = function () { // correct spelling
+    accelerate() {
         this.speed += 10;
         console.log(`${this.make} is going at ${this.speed} km/h`);
     }
 
-    brake = function () {
+    brake() {
         this.speed -= 5;
         console.log(`${this.make} is going at ${this.speed} km/h`);
+        return this;
     }
 
-
-
     get speedUS() {
-        return this.speed / 1.6
+        return this.speed / 1.6;
     }
 
     set speedUS(speed) {
-        this.speed = speed * 1.6
-
+        this.speed = speed * 1.6;
     }
 }
 
-const ford = new carCL('Ford', 120)
+class EVCl extends CarCl {
+    #charge;
+
+    constructor(make, speed, charge) {
+        super(make, speed);
+        this.#charge = charge;
+    }
+
+    chargeBattery(chargeTo) {
+        this.#charge = chargeTo;
+        return this;
+    }
+
+    accelerate() {
+        this.speed += 20;
+        this.#charge--;
+        console.log(
+            `${this.make} is going at ${this.speed} km/h, with a charge of ${this.#charge
+            }`
+        );
+        return this;
+    }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+// console.log(rivian.#charge);
+rivian
+    .accelerate()
+    .accelerate()
+    .accelerate()
+    .brake()
+    .chargeBattery(50)
+    .accelerate();
+
+console.log(rivian.speedUS);
+
