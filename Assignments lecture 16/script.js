bb18c8049d625ca78be2ce92e8965bfb1b940aff
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-10-23 14:31:39 +0200

    feat: add async image loading and coding challenge instructions in script.js
    - Implement an async function 'loadNPause' to handle image loading using async/await.
    - Create a new async function 'loadAll' to load multiple images from an array and apply CSS styles.
    - Update comments to provide clear instructions for the coding challenge and testing procedures.

--- Assignments lecture 16/script.js
+++ Assignments lecture 16/script.js
@@ -101,7 +101,7 @@ TEST DATA: Images in the img folder. Test the error handler by passing a wrong i
 Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
 
 GOOD LUCK 😀
-*/
+
 
 const btn = document.querySelector('.btn-country');
 const body = document.querySelector('body')
@@ -194,4 +194,45 @@ btn.addEventListener('click', function () {
 
 });
 
+// Coding Challenge #3
 
+
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). 
+Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 😀
+*/
+
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+
+    const img = document.createElement('img')
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      body.appendChild(img)
+      body.classList.add('hidden');
+      resolve(img)
+
+    })
+
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not loaded'));
+    })
+
+  })
+
+}
